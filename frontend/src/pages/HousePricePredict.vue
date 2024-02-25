dee9e74c340a0ee2e6505996982433f40dae3953
Timmy <timmy56887@icloud.com>
2024-02-25 23:17:52 +0800

    add house price predict preview

--- frontend/src/pages/HousePricePredict.vue
+++ frontend/src/pages/HousePricePredict.vue
@@ -1,12 +1,27 @@
 <template>
-    <HousePrice class="p-5"/>
+    <div>
+        <HousePricePreview v-if="showpreview" @changePage="changePage"/>
+        <HousePrice v-else @changePage="changePage" class="p-5"/>
+    </div>
 </template>
 <script>
 import HousePrice from '@/components/HousePrice/HousePrice.vue';
+import HousePricePreview from '@/components/HousePrice/HousePricePreview.vue';
 
 export default{
     components:{
-        HousePrice
+        HousePrice,
+        HousePricePreview
+    },
+    data(){
+        return{
+            showpreview: true,
+        }
+    },
+    methods:{
+        changePage(){
+            this.showpreview = !this.showpreview;
+        }
     }
 }
 
